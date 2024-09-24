169321a3c0f58a5a5eb8a9e0f36b5c43678b13f4
syjoe <csy5501@gmail.com>
2024-09-24 17:55:01 +0900

    ADD: to maintain Token while using this website

---
+++ src/api/axiosInstance.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+// Axios instance
+const axiosInstance = axios.create({
+    baseURL: 'http://127.0.0.1:8000',
+});
+
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('authToken');
+        if (token) {
+            config.headers['Authorization'] = 'Bearer ${token}';
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default axiosInstance;
