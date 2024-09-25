f37f721be2e4d25d15c7d61d2dd079491ad8c3db
syjoe <csy5501@gmail.com>
2024-09-25 14:00:16 +0900

    UPDATE: comments

--- src/components/authTokenLogin.js
+++ src/components/authTokenLogin.js
@@ -3,14 +3,18 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function LoginPage() {
+    // state variables to hold the user input and error messages
     const [usernameOrEmail, setUsernameOrEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
+    // redirect the user to a different page
     const navigate = useNavigate();
 
     const handleLogin = async () => {
         try {
+            // await (Asynchronous operation) and we wait for the response from the server
             const response = await axios.post("http://127.0.0.1:8000/auth/token/login/", {
+                // request payload contains
                 username_or_email: usernameOrEmail,
                 password: password,
             });
@@ -23,7 +27,6 @@ function LoginPage() {
             } else {
                 setError("Login Failed. Please try again.")
             }
-
         } catch (err) {
             setError("Invalid credentials. Please try again.");
             console.error("Error during login:", err);
@@ -31,6 +34,7 @@ function LoginPage() {
     };
 
     return (
+        // style align the form vertically and center
         <div style={{ display: 'flex', flexDirection: 'column', width: '300px', margin: '0 auto' }}>
             <h1>Login</h1>
             <input
