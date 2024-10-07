46ab5bb98da1f48205ec48f9a2544f7b1fa11f40
syjoe <csy5501@gmail.com>
2024-10-07 16:56:56 +0900

    UPDATE: style

--- src/utils/styles.js
+++ src/utils/styles.js
@@ -9,39 +9,6 @@ const styles = {
         borderRadius: '5px',
         cursor: 'pointer',
     },
-    // stockQuery
-    companyListContainer: {
-        maxHeight: "200px",
-        overflowY: "scroll",
-        border: "1px solid #ccc",
-        padding: "10px",
-        margin: "10px 0",
-        width: "300px",
-    },
-    companyItem: {
-        padding: "10px",
-        cursor: "pointer",
-        borderBottom: "1px solid #eee",
-        display: "flex",
-        justifyContent: "space-between",
-    },
-    selectedItem: {
-        backgroundColor: "#d3f9d8",
-    },
-    inputBox: {
-        width: "400px",
-        padding: "10px",
-        fontSize: "16px",
-        marginBottom: "10px",
-    },
-    button: {
-        padding: "10px 20px",
-        backgroundColor: "#4CAF50",
-        color: "#fff",
-        border: "none",
-        cursor: "pointer",
-        fontSize: "16px",
-    },
     // App.js
     navbar: {
         width: '100%',
